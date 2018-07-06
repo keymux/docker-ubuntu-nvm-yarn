18e5d5c3cfcd57a97de13a593ef9c96205d94a54
Henry Brown <henry@ibespwn.com>
2018-07-05 21:57:49 -0700

    Only install and use if necessary

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@
 
 if [ -z "${NODE_VERSION}" ]; then
   NODE_VERSION="$(nvm ls | grep -oE "v[0-9\.]+")"
-fi
+else
+  nvm install ${NODE_VERSION} > /dev/null
 
-nvm install ${NODE_VERSION} > /dev/null
-nvm use ${NODE_VERSION} > /dev/null
+  nvm use ${NODE_VERSION} > /dev/null
+fi
 
 exec "$@"
