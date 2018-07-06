f6fadec36405177a03598f0f983a1c5d4b5a0d26
Henry Brown <henry@ibespwn.com>
2018-07-05 22:01:36 -0700

    Added force override for clobbering versions

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,21 +6,30 @@ ROOT_DIR=$(realpath "${SCRIPTS_DIR}/..")
 
 . "${SCRIPTS_DIR}/lib.sh"
 
+FORCE=0
+
+case $@ in
+  -f|--force)
+    FORCE=1
+    ;;
+esac
+
 main() {
   V=$(node -e 'console.log(require("./package.json").version);')
   I=${DOCKER_IMAGE_NAME}
 
   describe=$(git describe --tags --dirty)
 
-  if node "${SCRIPTS_DIR}/prevent_clobber.js" \
-    && check_version $describe; then
+  if ! node "${SCRIPTS_DIR}/prevent_clobber.js" && [ ${FORCE} -ne 1 ]; then
+    echo "Refusing to clobber existing version $describe, try \`-f\` to override" >&2
+  elif check_version $describe; then
     yarn build
 
     docker push ${I}:latest && \
       docker tag ${I} ${I}:${V} && \
       docker push ${I}:${V}
   else
-    echo "Refusing to publish 'dirty' version $describe"
+    echo "Refusing to publish 'dirty' version $describe" >&2
   fi
 }
 
