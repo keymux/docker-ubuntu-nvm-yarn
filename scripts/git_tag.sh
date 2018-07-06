71f94524af92d8c9b07a812fe87357402c334b8b
Henry Brown <henry@ibespwn.com>
2018-07-06 00:04:47 -0700

    Added forcing for tags

--- scripts/git_tag.sh
+++ scripts/git_tag.sh
@@ -7,4 +7,11 @@ ROOT_DIR="$(realpath "${SCRIPTS_DIR}/..")"
 . "${SCRIPTS_DIR}/lib.sh"
 
 v=$(cat "${ROOT_DIR}/package.json" | jq -r ".version")
+
+case $@ in
+  -f|--force)
+    git tag -d "${v}" && git push origin ":${v}"
+    ;;
+esac
+
 git tag $v
