f4b8cc102fc728274e386dae11bb9d4d9601c839
Henry Brown <h@0ti.me>
2020-08-15 12:34:11 -0500

    Fix nvm.sh

--- nvm.sh
+++ nvm.sh
@@ -1,30 +1,48 @@
 #!/bin/bash
 
-if [ "${OVERRIDE_UID}" = "0" ] || [ "${OVERRIDE_GID}" = "0" ]; then
-  # Probably don't ever want to allow overriding to the root user or group
-  exit -1
-fi
+as_root() {
+  if [ "${OVERRIDE_UID}" = "0" ] || [ "${OVERRIDE_GID}" = "0" ]; then
+    # Probably don't ever want to allow overriding to the root user or group
+    exit -1
+  fi
 
-if [ -n "${OVERRIDE_UID}" ]; then
-  usermod -u "${OVERRIDE_UID}" "${WORKING_USER}"
-fi
+  if [ -n "${OVERRIDE_UID}" ]; then
+    usermod -u "${OVERRIDE_UID}" "${WORKING_USER}"
+  fi
 
-if [ -n "${OVERRIDE_GID}" ]; then
-  groupmod -g "${OVERRIDE_GID}" "${WORKING_GROUP}"
-fi
+  if [ -n "${OVERRIDE_GID}" ]; then
+    groupmod -g "${OVERRIDE_GID}" "${WORKING_GROUP}"
+  fi
+}
 
-su -l "${WORKING_USER}"
+as_user() {
+  . ${NVM}
 
-. ${NVM}
+  if [ -z "${NODE_VERSION}" ]; then
+    NODE_VERSION="$(nvm ls | grep -oE "v[0-9\.]+")"
+  else
+    nvm install ${NODE_VERSION} > /dev/null
+  fi
 
-if [ -z "${NODE_VERSION}" ]; then
-  NODE_VERSION="$(nvm ls | grep -oE "v[0-9\.]+")"
+  nvm use ${NODE_VERSION} > /dev/null
+
+  npm install -g yarn
+
+  eval "$@"
+}
+
+if [ "${UID}" = "0" ]; then
+  as_root "$@"
+
+  set -x
+
+  ARGS=""
+
+  for i in "$@"; do
+    ARGS="${ARGS} \"${i}\""
+  done
+
+  su "${WORKING_USER}" -c "${BASH_SOURCE[0]} ${ARGS}"
 else
-  nvm install ${NODE_VERSION} > /dev/null
+  as_user "$@"
 fi
-
-nvm use ${NODE_VERSION} > /dev/null
-
-npm install -g yarn
-
-eval "$@"
